6f0a494d3bf5537d84a3c06464e4fbfad7fbf66a
Malyadri Illuri <malyadri@raayansystems.com>
2020-06-17 13:02:20 +0530

    km-bbb-sdformat-prebuilt-minimalfs.sh updated

--- prebuilt/km-bbb-sdformat-prebuilt-minimalfs.sh
+++ prebuilt/km-bbb-sdformat-prebuilt-minimalfs.sh
@@ -1,17 +1,42 @@
 #!/bin/bash -e
 
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+NC='\033[0m'              # No Color
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+BRedU='\033[4;31m'         # Underline
+
+
+
 check_mmc () {
         FDISK=$(LC_ALL=C fdisk -l 2>/dev/null | grep "Disk ${media}:" | awk '{print $2}')
 
         if [ "x${FDISK}" = "x${media}:" ] ; then
                 echo ""
-                echo "I see..."
+                echo -e "${Green}I see...${NC}"
                 echo ""
                 echo "lsblk:"
                 lsblk | grep -v sr0
                 echo ""
                 unset response
-                echo -n "Are you 100% sure, on selecting [${media}] (y/n)? "
+                echo -en "${Green}Are you 100% sure, on selecting [${media}] (y/n)? ${NC}"
                 read response
                 if [ "x${response}" != "xy" ] ; then
                         exit
@@ -19,25 +44,23 @@ check_mmc () {
                 echo ""
         else
                 echo ""
-                echo "Are you sure? I Don't see [${media}], here is what I do see..."
+                echo -e "${Red}Are you sure? I Don't see [${media}], here is what I do see...${NC}"
                 echo ""
                 echo "lsblk:"
                 lsblk | grep -v sr0
                 echo ""
-                echo "Permission Denied. Run with sudo"
+                echo -e "${Green}Permission Denied. Run with sudo"
                 exit
-        fi                                                                                      
-                                                                                        
+        fi
 }
-
 unmount_all_drive_partitions () {
         echo ""
-        echo "Unmounting Partitions"
-        echo "-----------------------------"
+        echo -e "${Red}Unmounting Partitions"
+        echo -e "${Green}-----------------------------${NC}"
 
         NUM_MOUNTS=$(mount | grep -v none | grep "${media}" | wc -l)
 
-	for ((i=1;i<=${NUM_MOUNTS};i++))
+        for ((i=1;i<=${NUM_MOUNTS};i++))
         do
                 DRIVE=$(mount | grep -v none | grep "${media}" | tail -1 | awk '{print $1}')
                 umount ${DRIVE} >/dev/null 2>&1 || true
@@ -48,28 +71,26 @@ unmount_all_drive_partitions () {
 MLO_uboot_copy_sd()
 {
 
+        echo -e "${Green}-----------------------------${NC}"
         echo "Zeroing out Drive"
-        echo "-----------------------------"
-        	dd if=/dev/zero of=${media} bs=1M count=100 || drive_error_ro
-        	sync
-        	dd if=${media} of=/dev/null bs=1M count=100
-      		sync
-	echo Using dd to place bootloader on drive
+        echo -e "${Green}-----------------------------${NC}"
+                dd if=/dev/zero of=${media} bs=1M count=100 || drive_error_ro
+                sync
+                dd if=${media} of=/dev/null bs=1M count=100
+                sync
+        echo Using dd to place bootloader on drive
 
-echo -----------------------------
-echo "MLO: dd if=MLO of=/dev/sdc count=2 seek=1 bs=128k"
-	dd if=MLO of=${media} count=2 seek=1 bs=128k
-echo -----------------------------
+        echo -e "${Green}-----------------------------${NC}"
+        echo -e "${Red}MLO: dd if=MLO of=/dev/sdc count=2 seek=1 bs=128k${NC}"
+                dd if=MLO of=${media} count=2 seek=1 bs=128k
+        echo -e "${Green}-----------------------------${NC}"
 
-echo -----------------------------
-echo "u-boot.img: dd if=u-boot.img of=/dev/sdc count=4 seek=1 bs=384k"
-	dd if=u-boot.img of=${media} count=4 seek=1 bs=384k
-echo -----------------------------
-echo -----------------------------
+        echo -e "${Green}-----------------------------${NC}"
+        echo -e "${Red}u-boot.img: dd if=u-boot.img of=/dev/sdc count=4 seek=1 bs=384k${NC}"
+                dd if=u-boot.img of=${media} count=4 seek=1 bs=384k
+        echo -e "${Green}-----------------------------${NC}"
 
 sudo sfdisk --version
-
-
 sudo sfdisk ${media} <<-__EOF__
 4M,,L,*
 __EOF__
@@ -77,50 +98,56 @@ __EOF__
 sync
 
 
-        echo "Partition Setup:"
-        echo "-----------------------------"
+        echo -e "${Red}Partition Setup:${NC}"
+        echo -e "${Green}-----------------------------${NC}"
+        echo ""
+        echo ""
+        echo -e "${Green}-----------------------------${NC}"
         LC_ALL=C fdisk -l "${media}"
-        echo "-----------------------------"
+        echo -e "${Green}-----------------------------${NC}"
 
 
-	 echo "Formating with: "
-	#   sudo mkfs.ext4 -L rootfs ${media}1
-	sudo  mkfs.ext4  ${media}1 -L rootfs 
-	# sudo mkfs.ext4 -L rootfs ${DISK}1
-        echo "-----------------------------"
-        
+         echo -e "${Red}Formating with:${NC} "
+        #   sudo mkfs.ext4 -L rootfs ${media}1
+        sudo  mkfs.ext4  ${media}1 -L rootfs
+        # sudo mkfs.ext4 -L rootfs ${DISK}1
+        echo -e "${Green}-----------------------------"
+        echo -e "-----------------------------${NC}"
+
         sync
-	unmount_all_drive_partitions
+        unmount_all_drive_partitions
         sync
-
 }
 
 minimal_fs_copy_sd()
 {
-        echo "-----------------------------"
-	echo "Minimal File System Copy to SDCARD:$2"
-        echo "-----------------------------"
+        echo -e "${Green}-----------------------------${NC}"
+	echo -e "${Red}Minimal File System Copy to SDCARD:$2"
+        echo -e "${Green}-----------------------------${NC}"
 
 	sudo mkdir -p /mnt/rootfs
 	sudo mount ${media}1 /mnt/rootfs
+	echo
 	sudo tar -xvf minimal-rootfs-1.0.tar.gz
+	echo -e "${Red}sudo tar -xvf minimal-rootfs-1.0.tar.gz -C /mnt/rootfs/${NC}"
 	sudo tar -xvf minimal-rootfs-1.0.tar -C /mnt/rootfs/
+	sudo rm -rf minimal-rootfs-1.0.tar
 
 	sync
+	echo -e "{Red}syncing...${NC}"
 	unmount_all_drive_partitions
 }
 
 usage()
 {
-        echo "usage: sudo $(basename $0) --mmc /dev/sdX "
-	cat <<-__EOF__
+        echo -e "${BRed}usage: ${Green}sudo $(basename $0) --mmc /dev/sdX ${NC}${Red}"
+        cat <<-__EOF__
 
-		CAUTION:mightbe your harddisk FORMAT. Give proper Device name	
+                CAUTION:mightbe your harddisk FORMAT. Give proper Device name
 		Find sd card parition name with lsblk command and it replace with X.
-
 	__EOF__
-	
-	exit
+
+        exit
 }
 
 if [ -z "$1" ]; then
@@ -141,9 +168,7 @@ while [ ! -z "$1" ] ; do
         shift
 done
 
-
-
 unmount_all_drive_partitions
-#MLO_uboot_copy_sd
+MLO_uboot_copy_sd
 minimal_fs_copy_sd
 #
